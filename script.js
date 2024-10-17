0cb2606109af046bf825204f61ec309182bd1ace
mateo <mateo@syneforge.com>
2024-10-17 17:38:49 +0300

    Waves width fixed by using js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const burgerMenu = document.getElementById('header-burger-menu');
   const sidebar = document.getElementById('sidebar');
   const closeSidebar = document.getElementById('sidebar-close');
+  const backgroundWaves = document.getElementById('home-what-is-it-background-wrapper');
+
+  const calculateBackgroundWavesWidth = () => {
+    const bodyWidth = document.body.clientWidth;
+    backgroundWaves.style.width = `${bodyWidth}px`;
+  }
 
   burgerMenu.addEventListener('click', () => {
     sidebar.classList.add('sidebar-show');
@@ -10,4 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
   closeSidebar.addEventListener('click', () => {
     sidebar.classList.remove('sidebar-show');
   });
+
+  if (backgroundWaves) {
+    calculateBackgroundWavesWidth();
+    window.addEventListener('resize', calculateBackgroundWavesWidth);
+  }
 });
