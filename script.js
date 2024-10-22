3b332f2182037ff98b6764555f2f2fa5b0c9bbe4
mateo <mateo@syneforge.com>
2024-10-22 14:30:19 +0300

    Button up created

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+const SCROLL_SCREENS_TO_SHOW_BUTTON_UP = 1.5;
+
 document.addEventListener('DOMContentLoaded', () => {
   const pathname = window.location.pathname;
 
+  const buttonUp = document.getElementById('button-up');
   const burgerMenu = document.getElementById('header-burger-menu');
   const sidebar = document.getElementById('sidebar');
   const closeSidebar = document.getElementById('sidebar-close');
   const backgroundWaves = document.getElementById('home-what-is-it-background-wrapper');
+
+  const onScrollHandler = () => {
+    const isNeedShowButton = window.scrollY >= (window.innerHeight * SCROLL_SCREENS_TO_SHOW_BUTTON_UP);
+
+    if (isNeedShowButton) {
+      buttonUp.classList.add('button-up-show');
+    } else {
+      buttonUp.classList.remove('button-up-show');
+    }
+  }
+
+  const calculateBackgroundWavesWidth = () => {
+    const bodyWidth = document.body.clientWidth;
+    backgroundWaves.style.width = `${bodyWidth}px`;
+  }
+
+  window.addEventListener('scroll', onScrollHandler);
+
+  buttonUp.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 
   burgerMenu.addEventListener('click', () => {
     sidebar.classList.add('sidebar-show');
@@ -17,11 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   if (Boolean(backgroundWaves) && !pathname.includes('terms') && !pathname.includes('privacy')) {
-    const calculateBackgroundWavesWidth = () => {
-      const bodyWidth = document.body.clientWidth;
-      backgroundWaves.style.width = `${bodyWidth}px`;
-    }
-
     calculateBackgroundWavesWidth();
     window.addEventListener('resize', calculateBackgroundWavesWidth);
   }
